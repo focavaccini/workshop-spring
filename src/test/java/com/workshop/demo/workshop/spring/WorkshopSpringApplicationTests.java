5b1cb2d166ba0f9915a5b07df9a48cc08796d8c7
Fernando Cavaccini <fernandocavaccini@gmail.com>
2021-12-13 12:00:32 -0300

    Project created

---
+++ src/test/java/com/workshop/demo/workshop/spring/WorkshopSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workshop.demo.workshop.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
