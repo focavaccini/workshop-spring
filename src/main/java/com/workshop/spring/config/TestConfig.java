e387a53930a802944e41aef145251f49e269fcdd
Fernando Cavaccini <fernandocavaccini@gmail.com>
2021-12-14 17:17:45 -0300

    Payment, one-to-one association

--- src/main/java/com/workshop/spring/config/TestConfig.java
+++ src/main/java/com/workshop/spring/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.workshop.spring.entities.Category;
 import com.workshop.spring.entities.Order;
 import com.workshop.spring.entities.OrderItem;
+import com.workshop.spring.entities.Payment;
 import com.workshop.spring.entities.Product;
 import com.workshop.spring.entities.User;
 import com.workshop.spring.entities.enums.OrderStatus;
@@ -78,5 +79,9 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		orderRepository.save(o1);
 	}
 }
